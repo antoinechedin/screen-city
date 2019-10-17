ddc67784123b5f7571a740c8ed30385f5a5636a4
antoinechedin <antoine.chedin@gmail.com>
2019-10-17 11:49:48 +0200

    Add Size manipulation

--- Assets/Scripts/BuildingSystem.cs
+++ Assets/Scripts/BuildingSystem.cs
@@ -1,24 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BuildingSystem : MonoBehaviour
 {
     public GameObject prefab;
-    public LayerMask buildMask;
+    public LayerMask worldMask;
     public float buildDistance;
 
     GameObject currentObj;
+    float objSize = 3;
 
     void Update()
     {
+        bool updateSize = false;
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (objSize < 7)
+            {
+                objSize += 2f;
+                updateSize = true;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (objSize > 1)
+            {
+                objSize -= 2f;
+                updateSize = true;
+            }
+        }
+
+        if (updateSize)
+            UpdateObjSize();
+
         RaycastHit buildHit;
-        if (Physics.Raycast(transform.position, transform.forward, out buildHit, buildDistance, buildMask))
+        if (Physics.Raycast(transform.position, transform.forward, out buildHit, buildDistance, worldMask))
         {
             if (currentObj == null)
                 currentObj = Instantiate(prefab);
 
-            currentObj.transform.position = snapToGrid(buildHit.point + buildHit.normal * 0.1f);
+            SnapObjectToGrid(currentObj, buildHit);
         }
         else
         {
@@ -42,9 +66,22 @@ public class BuildingSystem : MonoBehaviour
         }
     }
 
-    Vector3 snapToGrid(Vector3 vec)
+    private void UpdateObjSize()
     {
+        if (currentObj != null)
+                    currentObj.transform.localScale = new Vector3(objSize, objSize, objSize);
+                prefab.transform.localScale = new Vector3(objSize, objSize, objSize);
+    }
+
+    private void SnapObjectToGrid(GameObject obj, RaycastHit hit)
+    {
+
         // TODO: For now it only snap to closest integer, change it with the grid system
-        return new Vector3(Mathf.Round(vec.x), Mathf.Round(vec.y), Mathf.Round(vec.z));
+        obj.transform.position = new Vector3(Mathf.Round(hit.point.x), Mathf.Round(hit.point.y), Mathf.Round(hit.point.z));
+        Vector3 colliderHalfSize = currentObj.transform.localScale / 2f - Vector3.one * 0.1f;
+        while (Physics.CheckBox(obj.transform.position, colliderHalfSize, Quaternion.identity, worldMask))
+        {
+            obj.transform.position += hit.normal;
+        }
     }
 }
